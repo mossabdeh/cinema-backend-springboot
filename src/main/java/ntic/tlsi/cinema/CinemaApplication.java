5c90ad729297cb8a5b512dba0f1940a272273456
Mossab dehane <mossabdehane19@gmail.com>
2023-06-17 20:34:39 +0200

    adding configuration in SpringBootApplication  that allow requests from FrontEnd

--- src/main/java/ntic/tlsi/cinema/CinemaApplication.java
+++ src/main/java/ntic/tlsi/cinema/CinemaApplication.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CinemaApplication  implements CommandLineRunner {
@@ -17,6 +20,17 @@ public class CinemaApplication  implements CommandLineRunner {
 
     @Autowired
     private RepositoryRestConfiguration restConfiguration;
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://cinema-frontend-angualr.netlify.app")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*");
+        }
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
